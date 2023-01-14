f7b579ba104e8241a5e0ee4b4bba7bd629293e62
dan_e <dan_eltsov@mail.ru>
2023-01-15 01:13:45 +0300

    is it work?

--- kop.cpp
+++ kop.cpp
@@ -1,10 +1,66 @@
-//
-// Created by 1 on 13.01.2023.
-//
-#include <iostream>
+/*#include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "NO";
-    return 0;
+cout « "Hello, World!" « std::endl;
+return 0;
+}*/
+
+
+/*#include <iostream>
+using namespace std;
+
+int main() {
+cout « 2 * 3 + 5;
+return 0;
+}*/
+
+
+/* // Программа для вывода а + b в красивом формате
+#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+cin » a » b;
+cout « a « " + " « b « " = " « a + b;
+return 0;
+}*/
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+cin » a » b;
+cout « a « " + " « b « " = " « a + b « endl;
+cout « a « " - " « b « " = " « a - b;
+return 0;
+} // endl переносит вывод строки на следующую
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+int v, t, g = 10;
+cin » t;
+v = g * t;
+int vm = v / 2;
+int s = vm * t;
+cout « "a thing flying: " « s;
+return 0;
 }
+*/
+
+
+// Программа для вывода округления в большую сторону
+// Но если число четное, то округления не будет
+
+/*#include <iostream>
+using namespace std;
+int main() {
+int a, b;
+cin » a » b;
+cout « (a + b - 1) / b;
+return 0;
+}*/
